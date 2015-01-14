43b7537deaf4483c28bbd9fe05dc27dee7cdbdf0
Em01 <emma_williams1@hotmail.co.uk>
2015-01-14 18:44:28 +0000

    generated model for categrory

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113181924) do
+ActiveRecord::Schema.define(version: 20150114184406) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "jobs", force: true do |t|
     t.string   "title"
